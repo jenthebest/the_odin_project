639592703bb51bf46bf4881caff12883c32529e2
Jen Best <jenthebest@gmail.com>
2014-11-24 11:59:43 -0500

    99 Bottles of beer loop

---
+++ learn_to_program/bottles_of_beer.rb
@@ -0,0 +1,9 @@
+#99 Bottles of Beer
+bottles = 99
+while bottles > 0
+	puts "#{bottles} bottles of beer on the wall,"
+	puts "#{bottles} bottles of beer,"
+	puts "Take one down, pass it around,"
+	bottles -= 1
+	puts "#{bottles} bottles of beer on the wall."
+end
